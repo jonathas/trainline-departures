63251512a5d71b60d18bf324e942c2d89503acff
Jon Ribeiro <contact@jonathas.com>
2017-11-26 14:10:36 +0100

    Configuring tests

---
+++ api/test/common.ts
@@ -0,0 +1,9 @@
+process.env.NODE_ENV = "test";
+
+import "mocha";
+
+const express = require("../config/express")();
+export const request = require("supertest")(express);
+
+export const chai = require("chai");
+export const should = chai.should();
